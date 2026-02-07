0f50d4fa186c9038018c4af450d3bcda694bd82f
Sushil Kushwaha <kushwahasushil204@gmail.com>
2026-02-07 14:17:02 +0530

    This is Creat React App(CRA) to push on inside react

---
+++ cra_app/src/App.js
@@ -0,0 +1,12 @@
+import Hello from "./Hello";
+
+function App() {
+  return (
+    <>
+    <Hello />
+    <h1>Sushil Kushwaha|Learning React</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ cra_app/src/Hello.js
@@ -0,0 +1,9 @@
+function Hello(){
+    return (
+        <>
+        <h1>Hello , How are You!</h1>
+        <h2>I Am Fine</h2>
+        </>
+    )
+}
+export default Hello
