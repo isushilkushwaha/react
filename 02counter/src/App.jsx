aa3b3cf2e524b3658f29885655002e32c060467f
Sushil Kushwaha <kushwahasushil204@gmail.com>
2026-02-07 14:08:13 +0530

    Added 02counter project inside react repo

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [counter,setCounter]= useState(0)
+
+  //let counter =23
+
+const add= ()=>{
+  if(counter>=25){}
+  else{
+setCounter(counter+2)
+  }
+}
+
+const remove =() =>{
+  if(counter<=0){}
+  else{
+  setCounter(counter-1)
+  
+  }}
+  return (
+    <>
+  <h1>Sushil</h1>
+  <h2>Counter: {counter}</h2>
+
+<button onClick={add}>Add Value</button>
+<br/>
+
+<button onClick={remove}>Remove Value </button>
+
+<p>counter:{counter}</p>
+
+  </>
+  )
+}
+
+export default App
