8e5c034a9a117daf2b433ff4fee7092b72df9753
Sushil Kushwaha <kushwahasushil204@gmail.com>
2026-02-11 01:50:22 +0530

    React route app

--- 07_React_route/src/components/Settings.jsx
+++ 07_React_route/src/components/Settings.jsx
@@ -1,10 +1,10 @@
 function Settings() {
-  return (
+return (
     <div>
-      <h2>⚙️ Settings Component</h2>
-      <p>Account settings here</p>
+    <h2>⚙️ Settings Component</h2>
+    <p>Account settings here</p>
     </div>
-  );
+);
 }
 
 export default Settings;
